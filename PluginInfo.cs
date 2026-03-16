da4b29b6d610ce1276838e65c63e0c00826d919b
unieagle <unieagle@gmail.com>
2026-03-16 11:37:52 -0700

    update doc

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@ namespace BattlefieldAnalysisBaseCollectSpaceJunk
     {
         public const string PLUGIN_GUID = "un1eagle.battlefieldanalysisbasecollectspacejunk";
         public const string PLUGIN_NAME = "战场分析基站收集太空残骸";
-        public const string PLUGIN_VERSION = "1.2.1";
+        public const string PLUGIN_VERSION = "1.2.2";
     }
 }
